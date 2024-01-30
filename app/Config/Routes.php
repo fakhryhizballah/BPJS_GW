9e2e31eb006b9b4a649c0dffcbc4895a24216b7e
fakhryhizballah <falehry88@gmail.com>
2024-01-30 17:38:13 +0700

    Update REST route to /api/bpjs/ in Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/rest', 'Rest::index');
+$routes->get('/api/bpjs/', 'Rest::index');
